bc6f5a91b27caac1d7dfa79f9c30b3ba7a437564
IlyaRapoport <ilja.rapoport@gmail.com>
2021-07-27 14:25:44 +0300

    add new project

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.example.test.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @Value("${server.port}")
-    private int serverPort;
-
     @GetMapping
     public ResponseEntity<String> getString() {
 
-         return ResponseEntity.ok("Hello World! Server.port="+serverPort);
+         return ResponseEntity.ok("Hello World!");
     }
 }
